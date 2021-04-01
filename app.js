10404d5f442069ab76f92b95e7a217fcf9ad353e
RyuuseiKang <ryuuseikang@naver.com>
2021-04-01 21:05:01 +0900

    path 추가

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ db_config.connect(conn);
 const express = require("express");
 const app = express();
 
+const path = require('path');
 app.set('views', path.join(__dirname, '/views')); 
 
 app.use(express.urlencoded({ extended: true }));
