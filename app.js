f68549c449e32a66c8af5e3e99cf05d1acca8fe0
RyuuseiKang <ryuuseikang@naver.com>
2021-04-03 20:34:58 +0900

    sql

--- app.js
+++ app.js
@@ -335,7 +335,7 @@ async function GetMyData(_cookie, res) {
           conn.query(sql, function (err, rows, fields) {
             if(err) console.log('query is not excuted. select fail...\n' + err);
           else {
-            console.log(rows[0]);
+            console.log(rows[sql]);
           }
           });
 
