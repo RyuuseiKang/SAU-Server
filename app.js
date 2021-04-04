9e29be9f5148087734779e1f3a8c0e0e5c98c87e
RyuuseiKang <ryuuseikang@naver.com>
2021-04-04 15:44:43 +0900

    token 적용

--- app.js
+++ app.js
@@ -339,8 +339,13 @@ async function GetMyData(_cookie, res) {
             if (rows[0][sql] == null) {
               var current_date = (new Date()).valueOf().toString();
               var random = Math.random().toString();
-              crypto.createHash('sha1').update(current_date + random).digest('hex');
-              conn.query("")
+              var hash = crypto.createHash('sha1').update(current_date + random).digest('hex');
+              conn.query("select registrationToken('" + userId + "', '" + hash + "');", function (err, rows, fields) {
+                if(err) console.log('query is not excuted. select fail...\n' + err);
+                else {
+
+                }
+              });
             }
           }
           });
