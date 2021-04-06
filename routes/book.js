2b78933063d385dcfe29d7577f8ed228614431c8
RyuuseiKang <ryuuseikang@naver.com>
2021-04-06 19:24:36 +0900

    book.js

--- routes/book.js
+++ routes/book.js
@@ -16,13 +16,14 @@ module.exports = (app) => {
         var hash = crypto.createHash('sha1').update(current_date + random).digest('hex');
         var sql = "registrationBook('" + hash + "', '" + req.query.isbn + "', '" + req.query.title + "', '" + req.query.price + "')";    
         conn.query("select " + sql + ";", function (err, rows, fields) {
-        if(err) {
-            console.log('query is not excuted. select fail...\n' + err);
-            ResponseBody = {isError: true};
-        }
-        else {
-            ResponseBody = {isError: false};
-        }
+            if(err) {
+                console.log('query is not excuted. select fail...\n' + err);
+                ResponseBody = {isError: true};
+            }
+            else {
+                ResponseBody = {isError: false};
+            }
+        });
 
         res.send(ResponseBody);
     });
