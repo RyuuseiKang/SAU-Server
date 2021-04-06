389a3359b684e415efcd37911dcd38cd11c2a9b2
RyuuseiKang <ryuuseikang@naver.com>
2021-04-06 19:37:36 +0900

    edit

--- routes/book.js
+++ routes/book.js
@@ -14,7 +14,7 @@ module.exports = (app) => {
         var current_date = (new Date()).valueOf().toString();
         var random = Math.random().toString();
         var hash = crypto.createHash('sha1').update(current_date + random).digest('hex');
-        var sql = "registrationBook('" + hash + "', '" + req.query.isbn + "', ST_GeomFromText('" + req.query.title + "'), '" + req.query.price + "')";    
+        var sql = "registrationBook('" + hash + "', '" + req.query.isbn + "', '" + req.query.title + "', '" + req.query.price + "')";    
         console.log(sql);
         conn.query("select " + sql + ";", function (err, rows, fields) {
             if(err) {
