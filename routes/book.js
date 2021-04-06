3c3e28e7867d978856fb872b07f9234e0aba1f9e
RyuuseiKang <ryuuseikang@naver.com>
2021-04-07 07:43:30 +0900

    rowDataPacket

--- routes/book.js
+++ routes/book.js
@@ -49,8 +49,9 @@ module.exports = (app) => {
                 console.log('query is not excuted. select fail...\n' + err);
                 ResponseBody = {isError: true};
             } else {
-                console.log(rows[0]);
-                ResponseBody = {rows[0]};
+                console.log(rows['RowDataPacket']);
+                rowDataPacket = rows['RowDataPacket'];
+                ResponseBody = {rowDataPacket};
             }
         });
 
