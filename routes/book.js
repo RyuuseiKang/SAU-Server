4b21ac42d179c042b3913c1aa5d6e5de0f2ab521
RyuuseiKang <ryuuseikang@naver.com>
2021-04-07 15:08:27 +0900

    수정

--- routes/book.js
+++ routes/book.js
@@ -51,7 +51,7 @@ module.exports = (app) => {
             } else {
                 console.log(rows);
                 rowDataPacket = rows.toString();
-                ResponseBody = {rowDataPacket};
+                ResponseBody = JSON.parse(JSON.stringify(rowDataPacket));
             }
         });
 
