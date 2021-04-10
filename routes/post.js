1c6f7264d70602750b365cedec9c01054f8f710b
RyuuseiKang <ryuuseikang@naver.com>
2021-04-10 12:04:22 +0900

    posting and view

--- routes/post.js
+++ routes/post.js
@@ -16,9 +16,28 @@ module.exports = (app) => {
 
     router.get('/write', async (req, res) => {
         var user_token = req.query.token;
+		var book_token = req.query.bookToken;
+		var isSell = req.query.isSell;
+		var description = req.query.description;
+		var major = req.query.major;
+		var price = req.query.price;
+		var image_token = req.query.imageToken;
 		jwt.verify(user_token, 'SeCrEtKeYfOrHaShInG',
 		function(err, decoded) {
-			res.send(decoded.user_token);
+			var current_date = (new Date()).valueOf().toString();
+			var random = Math.random().toString();
+			var hash = crypto.createHash('sha1').update(current_date + random).digest('hex');
+			
+			var sql = "select posting('" + hash + "', '" + decoded.user_token + "', '" + book_token + "', '" + isSell + "', '" + description + "', " + major + ", " + price + ", '" + image_token + "';";
+			conn.query(sql, function(err, rows, fields) {
+				if(err) console.log('query is not excuted. select fail...\n +' + err);
+				else {
+					res.send({
+						postToken: hash,
+					});
+				}
+			});
+
 		})
     });
 
